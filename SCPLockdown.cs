e4fbae8a3817dc9d62fe1146dc626ee4518afccb
Raul125 <71040879+Raul125@users.noreply.github.com>
2021-03-21 15:52:21 +0100

    New Version UwU

--- SCPLockdown.cs
+++ SCPLockdown.cs
@@ -31,7 +31,7 @@ namespace ScpLockdown
 
         public override string Prefix { get; } = "ScpLockdown";
 
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
 
