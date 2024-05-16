0c922a6616b26fbd3033c463300cc8c6c8703a03
Raul125 <raul125esp@gmail.com>
2024-05-16 20:44:07 +0200

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -13,7 +13,7 @@ public class Config : IConfig
     
     public bool Debug { get; set; } = false;
 
-    [Description("The affected SCPs, their shown text when unlocked and the time of their lockdown. (RoleType, string, int => RoleType, text, time in seconds)")]
+    [Description("The affected SCPs, the shown text when unlocked and the time of their lockdown. (RoleType, string, int => RoleType, text, time in seconds)")]
     public List<ScpLocker> AffectedScps { get; private set; } =
     [
         new(RoleTypeId.Scp079, "Containment Breach!", 60),
@@ -24,7 +24,7 @@ public class Config : IConfig
         new(RoleTypeId.Scp939, "Containment Breach!", 60)
     ];
 
-    [Description("Doors that you want to open/unlock/destroy/unlock after x seconds, this doors are locked at the round start. (DoorType, int, bool, bool, bool => DoorType, delay in seconds, unlock?, open?, destroy?)")]
+    [Description("Doors that you want to open/unlock/destroy/unlock after x seconds, this doors are locked at round start. (DoorType, int, bool, bool, bool => DoorType, delay in seconds, unlock?, open?, destroy?)")]
     public List<AffectedDoor> AffectedDoors { get; private set; } =
     [
         new(DoorType.CheckpointLczA, 60, true, false, false),
@@ -39,7 +39,7 @@ public class Config : IConfig
         new("containment breach detected All remaining personnel are advised to proceed with standard evacuation protocols", 120)
     ];
 
-    [Description("Can the Scp-079 use/switch cameras while is in lockdown?")]
+    [Description("Can the Scp-079 use/switch cameras while in lockdown?")]
     public bool Scp079Camera { get; private set; } = true;
 
     [Description("the plugin should use hints or broadcasts?.")]
